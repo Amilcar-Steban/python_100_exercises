a46de5826734b877f11bb954a28074c0f8610985
Amilcar-Steban <amilcarshm@gmail.com>
2024-03-07 04:14:10 -0500

    1

---
+++ 100exercises/suma.py
@@ -0,0 +1,8 @@
+"""
+suma de dos numeros y mostrar su resultado
+"""
+num1 = int(input("Ingresa un numero: "))
+num2 = int(input("Ingresa otro numero: "))
+result = num1+num2
+
+print("\nResultado: " f"{result}")
